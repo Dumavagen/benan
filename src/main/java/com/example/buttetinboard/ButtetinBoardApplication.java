e716eb235e52345c24ec6477884d3f155a657ea2
Dumavagen <dumavagen@gmail.com>
2021-01-14 15:47:12 +0300

    add async

--- src/main/java/com/example/buttetinboard/ButtetinBoardApplication.java
+++ src/main/java/com/example/buttetinboard/ButtetinBoardApplication.java
@@ -2,8 +2,10 @@ package com.example.buttetinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ButtetinBoardApplication {
 
     public static void main(String[] args) {
